b2917d3d89854cb65c31b7da80cb88bab4ee13ca
Lynix <81735347+lunarlynix@users.noreply.github.com>
2023-03-26 14:54:01 -0300

    Start using Gin

--- main.go
+++ main.go
@@ -1,21 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
-}
-
-func handleRequests() {
-	http.HandleFunc("/", homePage)
-	log.Fatal(http.ListenAndServe(":15494", nil))
-}
-
 func main() {
-	handleRequests()
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"notice":  "WARNING: This API is currently in development and is not ready for production use.",
+			"version": "LynixAPI v3.0.0_ALPHA (Matcha)",
+		})
+	})
+
+	// Default 404 Route
+	r.NoRoute(func(c *gin.Context) {
+		c.JSON(404, gin.H{
+			"error": "404 Not Found",
+		})
+	})
+
+	// Run on port 8557 which is default for LynixAPI
+	r.Run(":8557")
 }
