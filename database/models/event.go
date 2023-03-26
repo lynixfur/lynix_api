f17b2e5230beed625828a9d43c84966c6ee8bb19
Lynix <81735347+lunarlynix@users.noreply.github.com>
2023-03-26 15:35:10 -0300

    Create Models

--- database/models/event.go
+++ database/models/event.go
@@ -1,1 +1,13 @@
 package models
+
+import "gorm.io/gorm"
+
+type Event struct {
+	gorm.Model
+	EventName        string `json:"event_name"`
+	EventDescription string `json:"event_description"`
+	Location         string `json:"location"`
+	Status           string `json:"status"`
+	StartDate        string `json:"start_date"`
+	EndDate          string `json:"end_date"`
+}
--- database/models/wolfhr.go
+++ database/models/wolfhr.go
@@ -1,1 +1,12 @@
 package models
+
+import "gorm.io/gorm"
+
+type WolfHR struct {
+	gorm.Model
+	ID             string  `json:"string_id"`
+	NeosUsername   string  `json:"neos_username"`
+	VRChatUsername string  `json:"vrchat_username"`
+	HeartRate      int     `json:"heartrate"`
+	BatteryHealth  float64 `json:"battery_health"`
+}
