816e87eb77b2e7e19bb29791e6ff289020154906
Lynix <81735347+lunarlynix@users.noreply.github.com>
2023-03-27 14:56:54 -0300

    Update Model

--- database/models/event.go
+++ database/models/event.go
@@ -1,13 +1,21 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+	"gorm.io/gorm"
+)
 
 type Event struct {
 	gorm.Model
-	EventName        string `json:"event_name"`
-	EventDescription string `json:"event_description"`
-	Location         string `json:"location"`
-	Status           string `json:"status"`
-	StartDate        string `json:"start_date"`
-	EndDate          string `json:"end_date"`
+	ID               uuid.UUID `gorm:"type:uuid;primary_key;"`
+	EventName        string    `json:"event_name"`
+	EventDescription string    `json:"event_description"`
+	EventType        string    `json:"event_type"`
+	EventImage       string    `json:"event_image"`
+	Location         string    `json:"location"`
+	Status           string    `json:"status"`
+	StartDate        time.Time `json:"start_date"`
+	EndDate          time.Time `json:"end_date"`
 }
